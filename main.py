d09df5539614bc4e44007ed1862e2762732a6a95
bakkarrr <bakarbro123@gmail.com>
2024-04-03 07:25:27 +0500

    fixes the bugs and now its working

--- main.py
+++ main.py
@@ -2,36 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import random
+import keyboard
+from selenium.webdriver.common.keys import Keys
+
 
 # Configurations
-login_time = 30                # Time for login (in seconds)
-new_msg_time = 5              # Time for a new message (in seconds)
-send_msg_time = 5             # Time for sending a message (in seconds)
-country_code = 92              # Set your country code
-action_time = 2                # Set time for button click action
-image_path = "D:\\message_send\\whatsapp-bulk-messenger\\image.png"  # Absolute path to your image
+login_time = 20  # Time for login (in seconds)
+new_msg_time = 5  # Time for a new message (in seconds)
+send_msg_time = 5  # Time for sending a message (in seconds)
+country_code = 92  # Set your country code
+action_time = 2  # Set time for button click action
 
 # Create driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-def send_message(message):
-    input_box = driver.find_element_by_css_selector('div[data-tab="6"]')
-    input_box.send_keys(message)
-    input_box.send_keys(Keys.ENTER)
-
-
-# Encode Message Text
-with open("message.txt", "r") as file:
-    msg = file.read()
+text = "automate texting with python chatbot"
 
 # Open browser with default link
 link = "https://web.whatsapp.com"
 driver.get(link)
 time.sleep(login_time)
+
+
+
 
 # Loop Through Numbers List
 with open("numbers.txt", "r") as file:
@@ -39,20 +36,21 @@ with open("numbers.txt", "r") as file:
         num = n.rstrip()
         link = f"https://web.whatsapp.com/send/?phone={country_code}{num}"
         driver.get(link)
-        print("link is ", link)
-        print(msg)
+        time.sleep(new_msg_time)
+        # time.sleep(new_msg_time)
+        element = WebDriverWait(driver, 20).until(
+         EC.presence_of_element_located((By.CSS_SELECTOR, "._ak1l .xh8yej3 .x1hx0egp p.selectable-text.copyable-text.x15bjb6t.x1n2onr6"))
+)
+   
+        element.click()  
         time.sleep(new_msg_time)
         
-        # text = "To type text here"
-        # i think issue is in this loop
-        for line in msg:
-            actions = ActionChains(driver)
-            actions.send_keys(line)
-            print("this",line)
-            actions.key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
-            actions.send_keys(Keys.ENTER)
-            actions.perform()
-            time.sleep(send_msg_time)
+        for char in text:
+            keyboard.write(char,delay=0)
+              
+        keyboard.press_and_release('enter')
+        time.sleep(new_msg_time)
+        
 
 # Quit the driver
 driver.quit()
--- main2.py
+++ main2.py
@@ -1,52 +1,56 @@
-# Packages
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-import time
 
-# Config
-login_time = 80                 # Time for login (in seconds)
-new_msg_time = 20               # Time for a new message (in seconds)
-send_msg_time = 20              # Time for sending a message (in seconds)
-country_code = 92               # Set your country code
-action_time = 2                 # Set time for button click action
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.by import By
+
+
+# Configurations
+login_time = 30  # Time for login (in seconds)
+new_msg_time = 5  # Time for a new message (in seconds)
+send_msg_time = 5  # Time for sending a message (in seconds)
+country_code = 92  # Set your country code
+action_time = 2  # Set time for button click action
 
 # Create driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-# Encode Message Text
-with open('message.txt', 'r') as file:
-    msg = file.read()
-
 # Open browser with default link
-link = 'https://web.whatsapp.com'
+link = "https://web.whatsapp.com"
 driver.get(link)
 time.sleep(login_time)
 
-# List of contact names from EMS1 to EMS50
-contact_names = [f"EMS{i}" for i in range(1, 3)]
-
-# Loop Through Contact Names
-for contact_name in contact_names:
-    search_box = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="3"]')
-    search_box.clear()
-    search_box.send_keys(contact_name)
-    time.sleep(2)  # Wait for contact to appear in search results
-    # Click on the contact to open the chat
-    contact = driver.find_element_by_xpath(f'//span[@title="{contact_name}"]')
-    contact.click()
-    time.sleep(new_msg_time)
-    # Start the action chain to write the message
-    actions = ActionChains(driver)
-    for line in msg.split('\n'):
-        actions.send_keys(line)
-        # SHIFT + ENTER to create next line
-        actions.key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
-    actions.send_keys(Keys.ENTER)
-    actions.perform()
-    time.sleep(send_msg_time)
+text = "To type text here"
+# Loop Through Numbers List
+with open("numbers.txt", "r") as file:
+    for num in file.readlines():
+        num = num.strip()
+        link = f"https://web.whatsapp.com/send/?phone={country_code}{num}"
+        driver.get(link)
+        time.sleep(new_msg_time)
+        
+        # Find the input element
+        input_box = driver.find_element(By.CSS_SELECTOR, ".x6ikm8r")
+        
+        for char in text:
+            input_box.send_keys(char)
+        time.sleep(new_msg_time)
+        input_box.send_keys(Keys.ENTER)
+        
+        
+            
+        
+        
+   
+        
+        # Send the message
+        send_button = driver.find_element(By.CSS_SELECTOR, ".x1c4vz4f")
+        
+        send_button.click()
+        
+        time.sleep(send_msg_time)
 
 # Quit the driver
 driver.quit()
